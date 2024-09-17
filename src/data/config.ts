a64d70af6549d16631fb138b735ab4a6c9e7613a
ajeeq <haziqazfar@gmail.com>
2024-09-17 23:59:36 +0800

    chore: updated metadata

--- src/data/config.ts
+++ src/data/config.ts
@@ -11,7 +11,7 @@ const config = {
     },
   },
   meta: {
-    title: 'Haziq Azfar Johari - Computer Science Recent Graduate',
+    title: 'Muhammad Haziq - Computer Science Graduate',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales ac dui at vestibulum. In condimentum metus id dui tincidunt, in blandit mi vehicula.',
     faviconPath: '/src/assets/my-image.jpg',
